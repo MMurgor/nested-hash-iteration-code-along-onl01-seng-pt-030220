b4a76c79d1e254c133382e64cd8a03551dda5ae0
Romena Murgor <romenamurgor@gmail.com>
2020-03-16 05:43:02 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,8 +1,25 @@
-def remove_strawberry(contacts)
-  contacts.each do |person, contact_details_hash|
-    contact_details_hash.each do |attribute, data|
-      if attribute == :favorite_ice_cream_flavors
-        data.delete_if{|ice_cream| ice_cream == "strawberry"}
-      end
-    end
+require_relative 'spec_helper'
+require_relative '../lib/contacts.rb'
+
+
+describe "#remove_strawberry" do
+  let :contacts do
+    {
+      "Jon Snow" => {
+        name: "Jon",
+        email: "jon_snow@thewall.we",
+        favorite_ice_cream_flavors: ["chocolate", "vanilla"]
+      },
+      "Freddy Mercury" => {
+        name: "Freddy",
+        email: "freddy@mercury.com",
+        favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
+      }
+    }
   end
+
+  it "removes the strawberry ice cream from :favorite_ice_cream_flavors" do
+    result = remove_strawberry(contacts)
+    expect(result["Freddy Mercury"][:favorite_ice_cream_flavors]).to_not include("strawberry")
+  end
+end
